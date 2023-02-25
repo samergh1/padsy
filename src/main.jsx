85e008d74dd14f3f819345ada16c86fa2e14ca36
abrilsar <a.sar@correo.unimet.edu.ve>
2023-02-25 11:29:19 -0400

    table doctors

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HomePageUrl, RegisterPageUrl, LoginPageUrl } from "./constants/urls"
+import { HomePageUrl, RegisterPageUrl, LoginPageUrl, SearchPageurl } from "./constants/urls"
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage"
 import { RegisterPage } from "./pages/RegisterPage/RegisterPage"
 import { LoginPage } from "./pages/LoginPage/LoginPage"
 import './index.css'
+import { SearchPage } from './pages/SearchPage/SearchPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path={HomePageUrl} element={<HomePage/>} /> 
         <Route path={RegisterPageUrl} element={<RegisterPage/>} /> 
         <Route path={LoginPageUrl} element={<LoginPage/>} /> 
-
+        <Route path={SearchPageurl} element={<SearchPage/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export function SearchPage() {
+   return (
+     <div>
+        <div>
+            <p>Directory Search</p>
+            <div>
+                <image></image>
+                <image></image>
+                <button ></button>
+            </div>
+        </div>
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>Speciality</th>
+                        <th>Location</th>
+                        <th>Email</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+     </div>
+  )
+}

5db077ef3df8c46336efa449cb58f69da20208b3
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-03-04 09:21:47 -0400

    Save navbar

--- src/pages/LandingPage/components/NavBar.jsx
+++ src/pages/LandingPage/components/NavBar.jsx
@@ -32,7 +32,8 @@ export function NavBar() {
 
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            Padsy
+            {/* <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
           </a>
         </div>
 

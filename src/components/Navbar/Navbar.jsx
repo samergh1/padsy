9563a9244cef05846980b0390a5147b221ec7cf6
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-03-24 17:39:11 -0400

    No colors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-<<<<<<< HEAD
-import { UilSearch, UilCommentAltDots, UilAngleDoubleLeft, UilSignout} from '@iconscout/react-unicons'
-import logo from "../../assets/logo.svg"
-
-export function Navbar() {
-  const [open, setOpen] = useState(true);
-=======
 import { useNavigate } from "react-router";
-import { UilSearch, UilCommentAltDots, UilAngleDoubleLeft, UilSignout} from '@iconscout/react-unicons'
-import logo from "../../assets/logo.svg"
+import {
+  UilSearch,
+  UilCommentAltDots,
+  UilAngleDoubleLeft,
+  UilSignout,
+} from "@iconscout/react-unicons";
+import logo from "../../assets/logo.svg";
 import { logout } from "../../firebase/authentication/authentication";
 import { LoginPageUrl } from "../../constants/urls";
 
@@ -16,16 +14,11 @@ export function Navbar() {
   const [open, setOpen] = useState(true);
   const navigate = useNavigate();
 
->>>>>>> develop
   const Menus = [
     { title: "Search doctors", src: UilSearch },
-    { title: "My chats", src: UilCommentAltDots}
+    { title: "My chats", src: UilCommentAltDots },
   ];
 
-<<<<<<< HEAD
-  return (
-    <div className="flex h-screen">
-=======
   const handleLogout = async () => {
     logout();
     navigate(LoginPageUrl);
@@ -33,74 +26,67 @@ export function Navbar() {
 
   return (
     <div className="flex h-screen sticky top-0">
->>>>>>> develop
-        <div
-            className={`${
-            open ? "w-72" : "w-20 "
-            } bg-gray h-full p-5 pt-8 relative duration-300 flex-col justify-between`}
-        >
-            <UilAngleDoubleLeft 
-                className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+      <div
+        className={`${
+          open ? "w-72" : "w-20 "
+        } bg-gray h-full p-5 pt-8 relative duration-300 flex-col justify-between`}
+      >
+        <UilAngleDoubleLeft
+          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
                 border-2 rounded-full  ${!open && "rotate-180"}`}
-            onClick={() => setOpen(!open)}
-            />
-            <div className="flex gap-x-4 items-center">
-                <div className="w-16 h-16 bg-green rounded-md"></div>
-                    <h1
-                        className={`text-black origin-left font-medium text-xl duration-200 ${
-                        !open && "scale-0"
-                        }`}
-                    >
-                    @USUARIO
-                    </h1>
-            </div>
-            <div className="flex">
-                <ul className="pt-6 flex-col items-center" >
-                    {Menus.map((Menu, index) => (
-                    <li
-                        key={index}
-                        className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-black-300 text-sm items-center gap-x-4 
+          onClick={() => setOpen(!open)}
+        />
+        <div className="flex gap-x-4 items-center">
+          <div className="w-16 h-16 bg-green rounded-md"></div>
+          <h1
+            className={`text-black origin-left font-medium text-xl duration-200 ${
+              !open && "scale-0"
+            }`}
+          >
+            @USUARIO
+          </h1>
+        </div>
+        <div className="flex">
+          <ul className="pt-6 flex-col items-center">
+            {Menus.map((Menu, index) => (
+              <li
+                key={index}
+                className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-black-300 text-sm items-center gap-x-4 
                         ${Menu.gap ? "mt-9" : "mt-2"} ${
-                            index === 0 && "bg-light-white"
-                        } `}
-                        >
-                        <Menu.src/>
-                        <span className={`${!open && "hidden"} origin-left duration-200 not-italic font-normal text-base leading-6 flex items-center text-center text-black`}>
-                            {Menu.title}
-                        </span>
-                    </li>
-                ))}
-                </ul>
-            </div>
-<<<<<<< HEAD
-            <div className="mt-96 flex-col-reverse">
-                <img src = {logo}/>
-            </div>
-        </div>
-        <div className="h-screen flex-1 p-7">
-            <h1 className="text-2xl font-semibold ">Home Page</h1>
-        </div>
-=======
-            <div className="mt-40 flex-col-reverse">
-                <img src = {logo}/>
-            </div>
-
-            {/* Logout */}
-            <div className="flex gap-3 justify-end mt-8">
-                <span className="text-black">Logout</span>
-                <button
-                    type="button"
-                    onClick={handleLogout}
-                    className="rounded-full p-1 text-black"
+                  index === 0 && "bg-light-white"
+                } `}
+              >
+                <Menu.src />
+                <span
+                  className={`${
+                    !open && "hidden"
+                  } origin-left duration-200 not-italic font-normal text-base leading-6 flex items-center text-center text-black`}
                 >
-                    <UilSignout />
-                </button>
-            </div>
+                  {Menu.title}
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
-        {/* <div className="h-screen flex-1 p-7">
+        <div className="mt-40 flex-col-reverse">
+          <img src={logo} />
+        </div>
+
+        {/* Logout */}
+        <div className="flex gap-3 justify-end mt-8">
+          <span className="text-black">Logout</span>
+          <button
+            type="button"
+            onClick={handleLogout}
+            className="rounded-full p-1 text-black"
+          >
+            <UilSignout />
+          </button>
+        </div>
+      </div>
+      {/* <div className="h-screen flex-1 p-7">
             <h1 className="text-2xl font-semibold ">Home Page</h1>
         </div> */}
->>>>>>> develop
     </div>
   );
-};
+}

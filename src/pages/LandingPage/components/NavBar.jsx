924a91608cdb62e97cb9469ede1b826060d8256e
DanielRojas-hub <69920982+DanielRojas-hub@users.noreply.github.com>
2023-03-28 08:47:16 -0400

    get doctor info in card

--- src/pages/LandingPage/components/NavBar.jsx
+++ src/pages/LandingPage/components/NavBar.jsx
@@ -123,7 +123,7 @@ export function NavBar() {
           <div className="hidden ml-6 lg:flex">
             <a href="login" className="ml-3 sm:block">
               <button
-                onclick="login"
+                onClick="login"
                 type="button"
                 className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >

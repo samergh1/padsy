84dad020328d575f664c51b3a8a34f0934fde798
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-03-28 07:38:25 -0400

    Feedback id

--- src/pages/FeedbackPage.jsx
+++ src/pages/FeedbackPage.jsx
@@ -25,7 +25,9 @@ export function FeedbackPage() {
 
   async function createFeedback(data) {
     try {
-      return await addDoc(collection(db, "feedbacks"), data);
+      const reference = await addDoc(collection(db, "feedbacks"), data);
+      await updateDoc(doc(db, "feedbacks", reference.id), { id: reference.id });
+      return reference;
     } catch (error) {
       console.error("Error creating feedback ", error);
     }
@@ -56,9 +58,7 @@ export function FeedbackPage() {
     const a = {
       feedbacks: arrayUnion(reference.id),
     };
-    console.log("a");
     await updateUser(data.doctorId, a);
-    console.log("b");
     toast.success("Succesfully registered feedback :)");
     await timeout(4000);
     navigate(LandingPageUrl);
@@ -109,7 +109,6 @@ export function FeedbackPage() {
                 <textarea
                   name="message"
                   id="message"
-                  // rows={4}
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   placeholder="Write a review about the experience with your doctor..."
                   value={message}

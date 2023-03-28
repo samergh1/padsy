201cc21a68fb5b53dbe6136e96b4d9cb01527aa6
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-03-28 08:04:24 -0400

    View appointment

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -85,34 +85,54 @@ export function Appointment({
       <Toaster />
       {showModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div className="justify-center items-center flex mx-6 overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="border-0 rounded-lg shadow-2xl relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Your Appointment</h3>
+                  <h3 className="text-3xl font-semibold text-center">
+                    {user.name}'s Appointment
+                  </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none"></span>
-                  </button>
+                  ></button>
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    {date}
-                  </p>
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    {hour}
-                  </p>
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    {user.name}
-                  </p>
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    {selectedDoctor.name}
-                  </p>
+                  <div className="flex">
+                    <p className="my-2 mr-8 font-bold text-slate-500 text-lg leading-relaxed">
+                      Date:
+                    </p>
+                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                      {date}
+                    </p>
+                  </div>
+                  <div className="flex">
+                    <p className="my-2 mr-8 font-bold text-slate-500 text-lg leading-relaxed">
+                      Start time:
+                    </p>
+                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                      {hour}
+                    </p>
+                  </div>
+                  <div className="flex">
+                    <p className="my-2 mr-8 font-bold text-slate-500 text-lg leading-relaxed">
+                      Duration:
+                    </p>
+                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                      1 hour
+                    </p>
+                  </div>
+                  <div className="flex">
+                    <p className="my-2 mr-8 font-bold text-slate-500 text-lg leading-relaxed">
+                      Your doctor is:
+                    </p>
+                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                      {selectedDoctor.name}
+                    </p>
+                  </div>
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
@@ -137,7 +157,6 @@ export function Appointment({
               </div>
             </div>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
     </>
